bfe14b801c89ab2d0186020b49b5a1b62f6140f5
birbjam <41635757+birbjam@users.noreply.github.com>
2019-05-19 14:59:55 -0700

    Added welcome message, changed port number to one that now works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,36 @@
 var mysql = require('mysql')
 var inquirer = require('inquirer')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'nodeUser',
+    password: '',
+    database: 'bamazon_DB'
+})
+
+connection.connect(function (err) {
+    if (err) throw err
+    start()
+})
+
+function start() {
+    inquirer
+        .prompt({
+            name: 'welcome',
+            type: 'list',
+            message: 'Welcome to Bamazon! Would you like to browse our store?',
+            choices: ['YES', 'EXIT']
+        })
+        .then(function (answer) {
+            if (answer.welcome === 'YES') {
+                showItems()
+            } else {
+                connection.end()
+            }
+        })
+}
+
+function showItems() {
+    break
+}
