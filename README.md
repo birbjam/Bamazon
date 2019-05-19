a2c58dee7661d6f34ec35d80db4510b25f3a9ab8
birbjam <41635757+birbjam@users.noreply.github.com>
2019-05-19 14:47:00 -0700

    Database set up, additional files added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Bamazon
+
+A storefront CLI application.
+
+## Built With
+- Node.js
+- JavaScript
+- MySQL
--- seeds.sql
+++ seeds.sql
@@ -4,6 +4,11 @@ VALUES
     ("Lemon", "Food", 20, 0.50),
     ("Plantain", "Food", 10, 1.00),
     ("Marker", "Office Supplies", 10, 0.50),
-    ("Notebook", "Office Supplies", 10, 2.00),
-    ("Key Chain", "Misc", 10, 3.50),
-    ("Earl Grey Tea", "Tea and Coffee", 10, 2.50);
+    ("Notebook", "Office Supplies", 6, 2.00),
+    ("Key Chain", "Misc", 3, 3.50),
+    ("Scented Candle", "Home Decor", 5, 6.50),
+    ("Tea Lights, pack of 10", "Home Decor", 4, 3.50),
+    ("Peppermint Tea", "Tea and Coffee", 15, 3.50),
+    ("Game Console", "Electronics", 2, 350.75),
+    ("HDMI Cable", "Electronics", 7, 13.00),
+    ("Earl Grey Tea", "Tea and Coffee", 15, 2.50);
