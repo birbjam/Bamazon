be367da22238c13e163e3f873ed2b7c0965bfd4c
birbjam <41635757+birbjam@users.noreply.github.com>
2019-06-02 19:07:02 -0700

    Commented code and updated readme

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 
 A basic storefront CLI application that uses mySQL to store/edit inventory items which can be ordered by the user via the item id.
 
+- Customer View
+The user can choose to see the inventory and will be prompted to select an item using its ID.
+Once an item is selected, the user will be prompted to select the quantity of the item.
+Once the quantity is selected, it will be subtracted from the current quantity of that item.
+
+If a user selects a quantity that is over what is available, the application will prompt the user to try again.
+
 ## Built With
 - Node.js
 - JavaScript
 - MySQL
 - Inquirer
 - npm colors
+- Screencastify
 
 ## Video Demo
 - [Bamazon Demo Link](https://drive.google.com/file/d/1qrEFo2Gxd9be5mfDlK07OYWAVGsd0eSX/view?usp=sharing)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
+// Setting up dependencies
 var mysql = require('mysql')
 var inquirer = require('inquirer')
 var colors = require('colors')
 
+// Setting up a connection to the mySQL database
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -10,11 +12,15 @@ var connection = mysql.createConnection({
     database: 'bamazon_DB'
 })
 
+// Error handling for the database connection. If no error,
+// runs the application.
 connection.connect(function (err) {
     if (err) throw err
     start()
 })
 
+// The start function which prompts the user if they'd like
+// to look at the existing inventory
 function start() {
     inquirer
         .prompt({
@@ -23,6 +29,10 @@ function start() {
             message: 'Welcome to Bamazon!\n Would you like to browse our store?'.green,
             choices: ['YES', 'EXIT']
         })
+
+        // If the user answers yes, the inventory is displayed
+        // via the showItems function. If the user chooses to
+        // exit, the connection ends and exits the application.
         .then(function (answer) {
             if (answer.welcome === 'YES') {
                 showItems()
@@ -32,21 +42,27 @@ function start() {
         })
 }
 
+// Defining the showItems function that pulls from the 'items' table.
 function showItems() {
     connection.query('SELECT * FROM items', function(err, res) {
+        // Error handling.
         if (err) throw err;
 
         console.log("\nExisting Inventory: ");
-
+        
+        // Loops through the items in the table and displays each item.
         for(var i = 0; i < res.length; i++){
         console.log("---------------------------------------------------------------------------------")
         console.log("ID ".green + res[i].id + " | " + "Product: ".green + res[i].item_name + " | " + "Category: ".green + res[i].category + " | " + "Qty: ".green + res[i].quantity + " | " + "Price: ".green + "$" + res[i].price)
         console.log("---------------------------------------------------------------------------------");
         }
+        // Once the items table is displayed, runs the userPurchase function.
         userPurchase();
     })
 }
 
+// Defining the userPurchase function which asks the user first which item they would like
+// and then how many of the item.
 function userPurchase() {
     inquirer.prompt([
         {
@@ -61,7 +77,9 @@ function userPurchase() {
             message: 'How many would you like? '.green,
             filter: Number
         }
-    ]).then(function(input) {
+    ])
+    // Based on the input the user provides, the function selects that particular item.
+    .then(function(input) {
         var product = input.id;
         var inputQuantity = input.quantity;
         var queryDB = 'SELECT * FROM items WHERE ?';
@@ -74,18 +92,25 @@ function userPurchase() {
                 showItems();
             } else {
                 var itemData = data[0];
-
+                
+                // Conditional statement that checks to make sure there is enough of the item in stock.
                 if(inputQuantity <= itemData.quantity) {
                     console.log('Item is in stock! Order being placed.');
                     var updateQueryDB = 'UPDATE items SET quantity = ' + (itemData.quantity - inputQuantity) + ' WHERE id = ' + product;
 
+                    // Updates the quantity of the item in the table.
                     connection.query(updateQueryDB, function(err, data) {
                         if(err) throw err;
-
+                        
+                        // Lets the user know the order has been placed and adds up the total.
                         console.log('You order has been placed! \nThe total is: $' + itemData.price * inputQuantity);
+                        // Runs the exit function.
                         exit();
                     })
-                } else {
+                }
+                // If there is not enough quantity of stock, it will log a message and 
+                // run the showItems function again so that the user can select a valid amount. 
+                else {
                     console.log('Not enough items in stock, please modify your order.');
                     showItems();
                 }
@@ -94,6 +119,9 @@ function userPurchase() {
     })
 }
 
+
+// Exit function that prompts the user if they'd like to buy anything else or if they'd like
+// to exit the application.
 function exit() {
   inquirer
     .prompt({
@@ -102,10 +130,12 @@ function exit() {
       message: "Would you like to buy anything else?",
       choices: ["YES", "EXIT"]
     })
+    // If the user wants to buy additional things, the showItems function is run again.
     .then(function(answer) {
       if (answer.goodbye === "YES") {
         showItems();
       } else {
+          // If they choose EXIT, the connection ends.
         connection.end();
       }
     });
